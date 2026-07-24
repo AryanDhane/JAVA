1d680622057e05bd6b6f7718213d7a9d403e0c45
AryanDhane <dhanearyan150074@gmail.com>
2026-07-24 18:36:02 +0530

    Conditional statement

--- Java Basic/e4.java
+++ Java Basic/e4.java
@@ -7,12 +7,13 @@ public class e4 {
 
         if(age >= 18){
             System.out.println("You are an adult.");
+        } else  if(age >= 13){
+            System.out.println("You are a teenager.");
         } else {
-            System.out.println("You are a minor.");
+            System.out.println("You are a child.");
         }
     }
 }
-
 
 
 // output
