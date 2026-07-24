3d231a5e5347898f3bec31c78191abd4da8c88b9
AryanDhane <dhanearyan150074@gmail.com>
2026-07-24 09:32:58 +0530

    ..

---
+++ Java Basic/e1.java
@@ -0,0 +1,19 @@
+public class e1 {
+    public static void main(String[] args){
+
+    // OUTPUT     
+
+        // next line  println
+        System.out.println("Hello, World!");
+        
+        // next line  \n
+        System.out.print("Hello,,World! \n");
+    }
+    
+}
+
+
+// output
+
+// Hello, World!   
+// Hello,,World!
