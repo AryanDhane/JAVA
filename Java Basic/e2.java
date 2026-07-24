c23534fe813e9f9bb3ccbcec717c20c7d5c53f46
AryanDhane <dhanearyan150074@gmail.com>
2026-07-24 09:55:36 +0530

    ..

---
+++ Java Basic/e2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class e2 {
+    public static void main(String[] args){
+
+        // INPUT
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        System.out.println("You entered: " + number);
+
+    } 
+}
+
+// output
+
+// Enter a number: 2
+// You entered: 2
