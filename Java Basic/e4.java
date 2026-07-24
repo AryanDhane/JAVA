1d6e0180408e4d95ace5c49ba4d9debd2b8b4b77
AryanDhane <dhanearyan150074@gmail.com>
2026-07-24 18:27:19 +0530

    ..

--- Java Basic/e4.java
+++ Java Basic/e4.java
@@ -1,5 +1,19 @@
 public class e4 {
     public static void main(String[] args){
 
-        
+        // CONDITIONAL STATEMENTS
+
+        int age = 20;
+
+        if(age >= 18){
+            System.out.println("You are an adult.");
+        } else {
+            System.out.println("You are a minor.");
+        }
+    }
 }
+
+
+
+// output
+// You are an adult.
