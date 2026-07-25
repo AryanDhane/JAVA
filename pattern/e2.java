b9265b58dfb9f88bfd8bd5ef5466e7da162c979b
AryanDhane <dhanearyan150074@gmail.com>
2026-07-25 08:11:33 +0530

    Multiplaction table

---
+++ pattern/e2.java
@@ -0,0 +1,26 @@
+package pattern;
+
+public class e2 {
+    
+    public static void main(String[] args){
+
+        int n = 10;
+        for(int i=1; i<=10; i++){
+            System.out.print(i * n);
+            System.out.println();
+        }
+    }
+}
+
+
+// Output
+// 10
+// 20
+// 30
+// 40
+// 50
+// 60
+// 70
+// 80
+// 90
+// 100
