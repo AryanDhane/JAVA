fec3a0fb55687f9a69690fa16e8e9a8084a579f5
AryanDhane <dhanearyan150074@gmail.com>
2026-07-25 07:57:20 +0530

    Do while loop

---
+++ loops/e3.java
@@ -0,0 +1,21 @@
+package loops;
+
+public class e3 {
+    public static void main(String[] args){
+
+        int i = 0;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<=5);
+
+    }
+}
+
+// output
+// 0
+// 1
+// 2
+// 3
+// 4
+// 5
